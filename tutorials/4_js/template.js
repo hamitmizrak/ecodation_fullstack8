aac0e7dbad66db214e1a36a4fdb1a09e03753104
hamitmizrak <hamitmizrak@gmail.com>
2023-02-26 16:21:03 +0300

    prompt,operators

--- tutorials/4_js/template.js
+++ tutorials/4_js/template.js
@@ -23,18 +23,60 @@
 //let   (ES6)
 //const (ES6)
 
-var degiskenAdi="Hamit Mızrak";
-console.log(degiskenAdi);
-console.log(typeof degiskenAdi);
+//var degiskenAdi="Hamit Mızrak";
+// console.log(degiskenAdi);
+// console.log(typeof degiskenAdi);
 
-degiskenAdi=true;
-console.log(degiskenAdi);
-console.log(typeof degiskenAdi);
+// degiskenAdi=true;
+// console.log(degiskenAdi);
+// console.log(typeof degiskenAdi);
 
-degiskenAdi=44;
-console.log(degiskenAdi);
-console.log(typeof degiskenAdi);
+// degiskenAdi=44;
+// console.log(degiskenAdi);
+// console.log(typeof degiskenAdi);
 
-degiskenAdi=44.55;
-console.log(degiskenAdi);
-console.log(typeof degiskenAdi);
+// degiskenAdi=44.55;
+// console.log(degiskenAdi);
+// console.log(typeof degiskenAdi);
+
+// //Variable Common Rules
+// var $_degiskenAdi44="deneme";
+// console.log($_degiskenAdi44);
+
+// //u have to dont make
+// //%@44$_degiskenAdi
+
+// //whoisting
+// degisken=3>1;
+// var degisken;
+// console.log(degisken);
+
+// //variable initialization
+// var initial='"0"';
+
+//operatorler
+//+ - * / %
+var number1=10;
+var number2=2;
+// console.log(number1+number2);
+// console.log(number1-number2);
+// console.log(number1*number2);
+// console.log(number1/number2);
+// console.log(number1%number2);
+
+// = == ===
+// &&=VE ||=VEYA
+// ! =değil
+
+// var number=100.000;
+// console.log(number);
+
+// var number=10.000;
+// console.log(number)
+
+// let number2=10;
+// let number2=10;
+
+//Kullanıcıdan veri almak istersek
+let userData=prompt("Lütfen adınızı girinizi");
+console.log(userData);
