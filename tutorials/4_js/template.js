8363df2b35c439336dbf16e73f69a4af0e313ac0
hamitmizrak <hamitmizrak@gmail.com>
2023-02-26 15:53:32 +0300

    var,let,const

--- tutorials/4_js/template.js
+++ tutorials/4_js/template.js
@@ -1,1 +1,40 @@
-alert("Ben js")
+// alert("Ben js");
+// window.alert("Ben js2")
+
+// interpreter nedir ?
+// compiler nedir ?
+// OOP nedir ?
+// Betik dil nedir ?
+// Cross origin nedir ?
+// npm ile yarn arasındaki farklar ?
+// libraries ile framework ?
+
+/*
+
+*/ 
+// window.document.write("Ekranda göster");
+// window.console.log("Log");
+// window.console.warn("warning");
+// window.console.error("error");
+//camelCase 
+//PascalCase
+
+//var 
+//let   (ES6)
+//const (ES6)
+
+var degiskenAdi="Hamit Mızrak";
+console.log(degiskenAdi);
+console.log(typeof degiskenAdi);
+
+degiskenAdi=true;
+console.log(degiskenAdi);
+console.log(typeof degiskenAdi);
+
+degiskenAdi=44;
+console.log(degiskenAdi);
+console.log(typeof degiskenAdi);
+
+degiskenAdi=44.55;
+console.log(degiskenAdi);
+console.log(typeof degiskenAdi);
