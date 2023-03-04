49396c421fbfbf9040f26860eb5cad0792fa29f8
hamitmizrak <hamitmizrak@gmail.com>
2023-03-04 17:53:03 +0300

    Java

--- tutorials/4_js/template.js
+++ tutorials/4_js/template.js
@@ -488,6 +488,16 @@
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // debug atmak
+let data=()=>{
+    let number1=10,number2=20,number3;
+    number3=number1+number2;
+    console.log(number3);
+}
+
+data()
+
+
+
 // kullanıcıdan aldığımız isim soyisim (boşluk var)
 // ilk karakter göstersin sonraki kelimeleri masking (maskeleme yapsın)
 // eğer kullanıcı isim ve soyisimi ilk karakteri küçük girmişse büyük olsun mutlaka
@@ -512,6 +522,36 @@ let usernameAndSurnameMasking2 = () => {
     console.log(surname.toUpperCase());
 }
 usernameAndSurnameMasking2();
+
+/*
+Java ile yapıldı. 
+
+    public static void usernameAndSurnameMasking(){
+        String username,surname;
+        Scanner klavye=new Scanner(System.in);
+        System.out.println("Adınız ve Soyadınız");
+        String usernameAndSurname=klavye.nextLine();
+
+        username = usernameAndSurname.substring(0, usernameAndSurname.indexOf(" "));
+        for (int i = 1; i < username.length(); i++) {
+            username = username.replace(username.charAt(i), '*');
+        }
+        System.out.println(username.toUpperCase());
+
+
+        surname = usernameAndSurname.substring(usernameAndSurname.indexOf(" ") + 1, usernameAndSurname.length());
+        for (int i = 3; i < surname.length(); i++) {
+            surname = surname.replace(surname.charAt(i), '*');
+        }
+        System.out.println(surname.toUpperCase());
+    }
+
+    public static void main(String[] args) {
+        usernameAndSurnameMasking();
+    }
+}
+*/
+
 
 // software prensible DRY,KISS, YAGNI, Dummy Code,Clean Code
 // SOLID
