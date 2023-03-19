b609dd76f9525b6929bad378390d54d11db752fc
hamitmizrak <hamitmizrak@gmail.com>
2023-03-19 16:32:36 +0300

    back to top

--- web_design/template.js
+++ web_design/template.js
@@ -1,13 +1,24 @@
 
-
 // Dark mode 
 function dark_light() {
     let darkData = document.body.classList.toggle("dark_mode_css")
 }
 
-
 //now date 
-let now=()=>{
-document.getElementById("now_date").innerHTML=new Date().getFullYear();
+let now = () => {
+    document.getElementById("now_date").innerHTML = new Date().getFullYear();
 }
-now()
+now();
+
+//BackToTop 
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $("#back_top").fadeIn('slow');
+    } else {
+        $("#back_top").fadeOut('slow');
+    }
+});
+$("#back_top").click(function(){
+    $('html,body').animate({scrollTop:0},100,'easeInOutExpo');
+    return false;
+});
