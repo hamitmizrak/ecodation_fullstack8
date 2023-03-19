5d32263b673040c81b3efdfa8d9979bb801360d1
hamitmizrak <hamitmizrak@gmail.com>
2023-03-19 18:33:50 +0300

    validation

--- web_design/template.js
+++ web_design/template.js
@@ -5,14 +5,46 @@ function dark_light() {
 }
 
 //Search search_id
-$(function(){
-    const searchData=["Adana","Bursa","Diyarbakir","Elazığ","Giresun","Kastamonu","Malatya","Niğde","Van"];
-   $("#search_id").autocomplete({
-    source:searchData
-   });
+$(function () {
+    const searchData = ["Adana", "Bursa", "Diyarbakir", "Elazığ", "Giresun", "Kastamonu", "Malatya", "Niğde", "Van"];
+    $("#search_id").autocomplete({
+        source: searchData
+    });
 })
 
 // Register 
+// Eğer inputlar dolu ise validation yazı silinecek
+$(function () {
+    $("#registerSubmit").click(function () {
+        let nickname, email, passwd;
+        //nickname
+        nickname = $("#nickname").val();
+        nickname = jQuery.trim(nickname);
+
+        //email
+        email = $("#email").val();
+        email = jQuery.trim(email);
+
+        //nickname
+        passwd = $("#passwd").val();
+        passwd = jQuery.trim(passwd);
+
+        //validation nickname
+        if (nickname == "") {
+            $("#nicknameValidation").html(`<b>Nickname boş geçilemez</b>`)
+        }
+
+        //emailValidation
+        if (email == "") {
+            $("#emailValidation").html(`<b>email boş geçilemez</b>`)
+        }
+
+        //passwdValidation
+        if (passwd == "") {
+            $("#passwdValidation").html(`<b>passwd boş geçilemez</b>`)
+        }
+    });
+})
 
 
 // LOGIN ODEV
@@ -25,8 +57,8 @@ $(window).scroll(function () {
         $("#back_top").fadeOut('slow');
     }
 });
-$("#back_top").click(function(){
-    $('html,body').animate({scrollTop:0},100,'easeInOutExpo');
+$("#back_top").click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 100, 'easeInOutExpo');
     return false;
 });
 
@@ -36,11 +68,11 @@ $("#back_top").click(function(){
 let now = () => {
     //HTML
     document.getElementById("now_date").innerHTML = new Date().getFullYear();
-    
+
     // CSS
-    $("#now_date").css("color","red");
-    const cssDate={
-        "box-shadow":"1px 1px 3px 1px rgba(255,255,255,0.1)",
+    $("#now_date").css("color", "red");
+    const cssDate = {
+        "box-shadow": "1px 1px 3px 1px rgba(255,255,255,0.1)",
     }
     $("#now_date").css(cssDate);
 }
